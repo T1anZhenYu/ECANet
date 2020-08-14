4be3826b213aa239aa2d3625f663317c0c15c193
T1anZhenYu <18215595124@163.com>
2020-08-14 22:41:49 +0800

    cifar and NewBN model

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,5 @@
 from .eca_resnet import *
 from .eca_mobilenetv2 import *
+
+from .mobilenetv2 import *
+from .resnet import *
--- models/eca_module.py
+++ models/eca_module.py
@@ -29,4 +29,109 @@ class eca_layer(nn.Module):
         y = self.sigmoid(y)
 
         return x * y.expand_as(x)
+
+
+class NewBN_MeanOnly(nn.Module):
+
+    def __init__(self, num_features, eps=1e-05, momentum=0.9, affine=True):
+        super(NewBN_MeanOnly, self).__init__()
+
+        t = int(num_features / 8)
+        ks = t if t % 2 else t + 1
+
+        self.linearvar = nn.Conv1d(1, 1, kernel_size=ks, padding=(ks - 1) // 2, bias=False)
+        self.linearmean = nn.Conv1d(1, 1, kernel_size=ks, padding=(ks - 1) // 2, bias=False)
+
+        self.sigmoid = nn.Sigmoid()
+        self.bn = nn.BatchNorm2d(num_features)
+
+    def forward(self, x):
+        n = x.numel() / (x.size(1))
+        if self.training:
+
+            mean = x.mean(dim=(0, 2, 3))
+            # var = (x-mean[None, :, None, None]).pow(2).mean(dim=(0,2, 3))
+
+            # indexvar = torch.sqrt(self.bn.running_var).mean()/math.pow(n,0.5)
+            indexmean = self.bn.running_mean.mean() / math.pow(n, 0.5)
+
+            # varmix = indexvar * torch.sqrt(var)
+            # varmix = self.sigmoid(self.linearvar(varmix[None,None,:]).squeeze())
+
+            meanmix = indexmean * mean
+
+            meanmix = self.sigmoid(self.linearmean(meanmix[None, None, :]).squeeze())
+
+            index = meanmix
+
+        else:
+            mean = self.bn.running_mean
+            var = self.bn.running_var
+
+            # indexvar = torch.sqrt(self.bn.running_var).mean()/math.pow(n,0.5)
+            indexmean = self.bn.running_mean.mean() / math.pow(n, 0.5)
+
+            # varmix = indexvar * torch.sqrt(var)
+            # varmix = self.sigmoid(self.linearvar(varmix[None,None,:]).squeeze())
+
+            meanmix = indexmean * mean
+
+            meanmix = self.sigmoid(self.linearmean(meanmix[None, None, :]).squeeze())
+            index = meanmix
+
+        out = self.bn(x)
+        out.mul_(index[None, :, None, None])
+        return out
+
+
+class NewBN(nn.Module):
+
+    def __init__(self, num_features, eps=1e-05, momentum=0.9, affine=True):
+        super(NewBNy, self).__init__()
+
+        t = int(num_features / 8)
+        ks = t if t % 2 else t + 1
+
+        self.linearvar = nn.Conv1d(1, 1, kernel_size=ks, padding=(ks - 1) // 2, bias=False)
+        self.linearmean = nn.Conv1d(1, 1, kernel_size=ks, padding=(ks - 1) // 2, bias=False)
+
+        self.sigmoid = nn.Sigmoid()
+        self.bn = nn.BatchNorm2d(num_features)
+
+    def forward(self, x):
+        n = x.numel() / (x.size(1))
+        if self.training:
+
+            mean = x.mean(dim=(0, 2, 3))
+            var = (x-mean[None, :, None, None]).pow(2).mean(dim=(0,2, 3))
+
+            indexvar = torch.sqrt(self.bn.running_var).mean()/math.pow(n,0.5)
+            indexmean = self.bn.running_mean.mean() / math.pow(n, 0.5)
+
+            meanmix = indexmean * mean
+            meanmix = self.sigmoid(self.linearmean(meanmix[None, None, :]).squeeze())
+
+            varmix = indexvar * torch.sqrt(var)
+            varmix = self.sigmoid(self.linearvar(varmix[None,None,:]).squeeze())
+
+            index = meanmix*0.5 + varmix*0.5
+
+        else:
+            mean = self.bn.running_mean
+            var = self.bn.running_var
+
+            indexvar = torch.sqrt(self.bn.running_var).mean()/math.pow(n,0.5)
+            indexmean = self.bn.running_mean.mean() / math.pow(n, 0.5)
+
+            meanmix = indexmean * mean
+            meanmix = self.sigmoid(self.linearmean(meanmix[None, None, :]).squeeze())
+
+            varmix = indexvar * torch.sqrt(var)
+            varmix = self.sigmoid(self.linearvar(varmix[None,None,:]).squeeze())
+
+            index = meanmix*0.5 + varmix*0.5
+
+        out = self.bn(x)
+        out.mul_(index[None, :, None, None])
+        return out
         
