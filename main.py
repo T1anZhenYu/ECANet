1ca42287a0ebaf11bc1cb2461cf6e0c2825797a3
T1anZhenYu <18215595124@163.com>
2020-08-15 15:48:00 +0000

    convert layer

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ import torch.utils.data.distributed
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 import models
-
+from models.eca_module import *
 model_names = sorted(name for name in models.__dict__
     if name.islower() and not name.startswith("__")
     and callable(models.__dict__[name]))
