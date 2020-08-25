74583f78b539cb41361fc0ddbfe605a133a6d802
T1anZhenYu <18215595124@163.com>
2020-08-25 02:49:22 +0000

    eca modify

--- models/eca_module.py
+++ models/eca_module.py
@@ -12,7 +12,7 @@ class eca_layer(nn.Module):
     def __init__(self, channel, k_size=3):
         super(eca_layer, self).__init__()
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        t = int(abs((math.log(num_features, 2) + 1) / 2))
+        t = int(abs((math.log(channel, 2) + 1) / 2))
         k_size = t if t % 2 else t + 1
         self.conv = nn.Conv1d(1, 1, kernel_size=k_size, padding=(k_size - 1) // 2, bias=False) 
         self.sigmoid = nn.Sigmoid()
