69d9c8e72925c22c37ac3f8fbf76fa865dfa4a55
T1anZhenYu <18215595124@163.com>
2020-08-26 21:05:13 +0800

    modelCompare

---
+++ modelCompare.py
@@ -0,0 +1,9 @@
+import torch
+from torchvision import models
+from torchsummary import summary
+from models import resnet
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+resnet18_origin = models.resnet18().to(device)
+resnet18_eca = resnet.resnet18(1000)
+summary(resnet18_origin, (3, 224, 224))
+summary(resnet18_eca,(3,224,224))
